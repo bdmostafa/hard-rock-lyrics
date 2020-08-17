3183c182703630b410425c1639532e68cb70c67f
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-17 10:47:28 +0600

    data shorted and return functionality as needed from the whole big data

--- js/app.js
+++ js/app.js
@@ -14,16 +14,58 @@ class Lyrics {
         for (let i = 0; i < 10; i++) {
             data.push(responseData.data[i]);
         }
-        console.log(data);
-        // As data is array object, return data through destructuring with the help of rest operator
+        // console.log(data);
+        // As data is array object, return data through destructuring with the help of spread operator
         return [...data];
     }
-
+    // Short the data as needed from the whole response data
+    getShortedData(data) {
+        const title = [];
+        const albumTitle = [];
+        const albumCoverImg = [];
+        const artistName = [];
+        const artistPic = [];
+        const duration = [];
+        const listen = [];
+        const lyrics = [];
+        data.forEach(data => {
+            // console.log(data)
+            title.push(data.title);
+            albumTitle.push(data.album.title);
+            albumCoverImg.push(data.album.cover);
+            artistName.push(data.artist.name);
+            artistPic.push(data.artist.picture);
+            duration.push(`${data.duration/1000}s`);
+            listen.push(data.preview);
+            lyrics.push(data.lyrics);
+        })
+        // Return the shorted data as an object
+        return {
+            song_title: title,
+            album_title: albumTitle,
+            album_cover: albumCoverImg,
+            artist_name: artistName,
+            artist_pic: artistPic,
+            song_duration: duration,
+            song_listen: listen,
+            song_lyrics: lyrics
+        }
+    }
 
 
 }
 
+// Handle UI part of Lyrics app =================================
+class UI {
+    constructor() {
+        // All the selectors
 
+    }
+    // Frontend/UI section - all the data passing here through DOM
+    paint() {
+
+    }
+}
 
 
 // Trigger DOM loaded
@@ -36,12 +78,18 @@ class Lyrics {
 // Instantiate lyrics
 const lyrics = new Lyrics;
 
-
+// Instantiate ui
+const ui = new UI;
 
 document.getElementById('search-input').addEventListener('keyup', (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    // Search the song as searchText value
     lyrics.searchLyrics(e.target.value)
         .then(data => {
-            console.log(data)
+            // Short the data as requirement/needed
+            const d = lyrics.getShortedData(data)
+
+            console.log(d)
+
         })
 })
